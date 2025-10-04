ca4da3b1f713330822282b1117044b4bd124b938
smarquezdiaz <smarquez@servicios.mc4.com.bo>
2025-10-03 21:07:26 -0400

    increase in timeout

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -10,6 +10,6 @@ def test_login(page):
     login.login(USERNAME, PASSWORD)
     page.wait_for_url("https://app.qase.io/projects")
     projects_heading_selector = "h1:has-text('Projects')"
-    page.wait_for_selector(projects_heading_selector, timeout=15000)
+    page.wait_for_selector(projects_heading_selector, timeout=30000)
     projects_heading = page.get_by_role("heading", name="Projects")
     expect(projects_heading).to_be_visible()
