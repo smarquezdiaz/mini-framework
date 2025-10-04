4eaf2c7ca54a1ae90ef236c2290d7dedd3a94e91
smarquezdiaz <smarquez@servicios.mc4.com.bo>
2025-10-03 20:38:07 -0400

    login

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -1,4 +1,15 @@
-import os
+from pages.login_page import LoginPage
+from utils.config import PASSWORD, USERNAME
+from utils.helpers import assert_visible
+from playwright.sync_api import expect
 
-BASE_URL = os.getenv('BASE_URL1','https://www.saucedemo.com/')
-HEADLESS = os.getenv('HEADLESS','True').lower() == 'true'
+
+def test_login(page):
+    login = LoginPage(page)
+    login.open()
+    login.login(USERNAME, PASSWORD)
+    page.wait_for_url("https://app.qase.io/projects")
+    projects_heading_selector = "h1:has-text('Projects')"
+    page.wait_for_selector(projects_heading_selector, timeout=15000)
+    projects_heading = page.get_by_role("heading", name="Projects")
+    expect(projects_heading).to_be_visible()
---
+++ utils/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+BASE_URL = os.getenv("BASE_URL1", "https://www.saucedemo.com/")
+HEADLESS = os.getenv("HEADLESS", "false").lower() == "true"
+TOKEN = os.getenv("TOKEN", "placeholder_token_for_safety")
+USERNAME = os.getenv("USERNAME", "placeholder_user@example.com")
+PASSWORD = os.getenv("PASSWORD", "placeholder_password")
+BASE_URL_API = os.getenv("BASE_URL_API", "https://api.default.com/v1/")
+SUITE = os.getenv("SUITE", "default_suite_name")
